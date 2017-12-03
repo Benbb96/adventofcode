7d89e7457ddccc41154e2ddff4ace17afb1dcfad
Benbb96 <benbb96@gmail.com>
2017-12-03 19:12:16 +0100

    2016 - Day1 Terminé

--- python/2016/day1/day1.py
+++ python/2016/day1/day1.py
@@ -4,29 +4,35 @@ class Me:
         self.x = 0
         self.y = 0
         self.direction = 'N'
+        self.historic = [(0, 0)]
+        self.firstTwiceVisitedPosition = (0, 0)
 
     def move(self, instruction):
-        print(self.direction + ' - ' + instruction)
-        turn = instruction[0]
-        step = int(instruction[1])
+        # print(self.direction + ' - ' + instruction)
+        turn = instruction[0]  # Le premier caractère est le sens dans lequel on se tourne
+        steps = int(instruction[1:])  # Les caractères suivants sont le nombre de pas à faire
 
         if turn == 'R':
-            self.turnRight()
+            self.turn_right()
         else:
-            self.turnLeft()
+            self.turn_left()
+        for step in range(steps):
+            if self.direction == 'N':
+                self.y += 1
+            elif self.direction == 'S':
+                self.y -= 1
+            elif self.direction == 'E':
+                self.x += 1
+            else:
+                self.x -= 1
+            position = (self.x, self.y)
+            if self.historic.__contains__(position) and self.firstTwiceVisitedPosition == (0, 0):
+                self.firstTwiceVisitedPosition = position
+            self.historic.append(position)
 
-        if self.direction == 'N':
-            self.y += step
-        elif self.direction == 'S':
-            self.y -= step
-        elif self.direction == 'E':
-            self.x += step
-        else:
-            self.x -= step
+        # print('x=' + str(self.x) + ' ; y=' + str(self.y) + '\n')
 
-        print('x=' + str(self.x) + ' ; y=' + str(self.y))
-
-    def turnRight(self):
+    def turn_right(self):
         if self.direction == 'N':
             self.direction = 'E'
         elif self.direction == 'E':
@@ -36,7 +42,7 @@ class Me:
         else:
             self.direction = 'N'
 
-    def turnLeft(self):
+    def turn_left(self):
         if self.direction == 'N':
             self.direction = 'W'
         elif self.direction == 'W':
@@ -47,18 +53,24 @@ class Me:
             self.direction = 'N'
 
 
-
-def day1(inputContent):
+def day1(input_content):
     me = Me()
-    instructions = inputContent.split(', ')
+    instructions = input_content.split(', ')
     for instruction in instructions:
         me.move(instruction)
-    distance = abs(me.x) + abs(me.y)
-    return distance
+    distance = get_distance((me.x, me.y))
+    return distance, me.firstTwiceVisitedPosition
+
+
+def get_distance(pos):
+    return abs(pos[0]) + abs(pos[1])
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         content = f.read()
-    print(content)
-    print('La distance parcouru est de ' + str(day1(content)))
+    # distance, position = day1('R8, R4, R4, R8')
+    distance, position = day1(content)
+    distanceFirstPosition = get_distance(position)
+    print('La distance parcouru est de ' + str(distance))
+    print('La première position visitée deux fois est ' + str(position) + ', soit à ' + str(distanceFirstPosition))
