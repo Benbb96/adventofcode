3c55229b54f5be5e9ab392decb830dc1e7525e15
Benbb96 <benbb96@gmail.com>
2017-12-12 01:34:03 +0100

    Fin Day 3

--- python/2017/day3/day3.py
+++ python/2017/day3/day3.py
@@ -1,158 +1,65 @@
-size = 5
-matrix = [[0 for i in range(size)] for i in range(size)]
+
+class Spiral:
+
+    def __init__(self, x, y, value, parent=None):
+        self.x = x
+        self.y = y
+        self.value = value
+        self.prev = parent
+
+    def __str__(self):
+        return 'x = ' + str(self.x) + '\t y = ' + str(self.y) + '\t value = ' + str(self.value)
 
 
-def fillMatrix():
-    positionX = int(size/2)
-    positionY = int(size / 2)
-    count = 1
-    width = 1
-    height = 1
+def day3(n):
+    x = y = 0
+    initial = Spiral(x, y, 1)  # Première case exemple
+    last = initial
+    direction = 'E'
+    moveX = 0
+    moveY = 0
+    widthLimit = 1
+    heightLimit = 1
+    for i in range(2, n + 1):  #On va créer toute sles cases jusqu'à celle ciblée
 
-    while count <= 20:
-        print('X=' + str(positionX) + ', Y= ' + str(positionY))
-        for w in range(width):
-            matrix[positionX][positionY] = count
-            count += 1
-            positionY += 1
-        width += 1
-        print('X=' + str(positionX) + ', Y= ' + str(positionY))
-        for h in range(height):
-            matrix[positionX][positionY] = count
-            count += 1
-            positionX -= 1
-        height += 1
-        print('X=' + str(positionX) + ', Y= ' + str(positionY))
-        for w in range(width):
-            matrix[positionX][positionY] = count
-            count += 1
-            positionY -= 1
-        width += 1
-        print('X=' + str(positionX) + ', Y= ' + str(positionY))
-        for h in range(height):
-            matrix[positionX][positionY] = count
-            count += 1
-            positionX += 1
-        height += 1
-        print('X=' + str(positionX) + ', Y= ' + str(positionY))
-
-
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY -= 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionX += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-    # count += 1
-    # positionY += 1
-    #
-    # matrix[positionX][positionY] = count
-
-
-def displayMatrix():
-    x = 0
-    while x < size:
-        y = 0
-        while y < size:
-            print(matrix[x][y], end='\t')
+        if direction == 'E':
             y += 1
-        print()
-        x += 1
+            moveY += 1
+        elif direction == 'N':
+            x -= 1
+            moveX += 1
+        elif direction == 'W':
+            y -= 1
+            moveY += 1
+        elif direction == 'S':
+            x += 1
+            moveX += 1
 
+        if moveY == widthLimit:
+            widthLimit += 1
+            moveY = 0
+            if direction == 'E':
+                direction = 'N'
+            elif direction == 'W':
+                direction = 'S'
 
-def day3(input):
-    pass
-    # print(input)
+        if moveX == heightLimit:
+            heightLimit += 1
+            moveX = 0
+            if direction == 'N':
+                direction = 'W'
+            elif direction == 'S':
+                direction = 'E'
+        
+        new = Spiral(x, y, i, last)
+        last.next = new
+        last = new
+
+    # Enfin, on retourne la distance de la case par rapport à l'origine
+    return abs(last.x) + abs(last.y)
 
 
 if __name__ == "__main__":
-    fillMatrix()
-    displayMatrix()
-    day3(1)
+    n = 289326
+    print('La distance à laquelle se trouve la case ' + str(n) + ' est de ' + str(day3(n)))
+    # La partie 2 s'est faite facilement grâce à OEIS : https://oeis.org/A141481
