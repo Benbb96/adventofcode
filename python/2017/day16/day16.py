ac4da771ef5026b9a61927c04bd2fa36eefd81c0
Benbb96 <benbb96@gmail.com>
2017-12-19 19:27:36 +0100

    Finish Day 16

--- python/2017/day16/day16.py
+++ python/2017/day16/day16.py
@@ -1,48 +1,64 @@
-s = 'abcdefghijklmnop'
 instructions = 's1,x3/4,pe/b'
 
 def rotate(strg,n):
     return strg[n:] + strg[:n]
 
 
-def first(input):
-    test = 'abcde'
+def first(input, test):
+
     for instruction in input:
         if instruction[0] == 's':
-            loop = int(instruction[1])
-            print('Spin ' + str(loop))
+            loop = int(instruction[1:])
+            #print('Spin ' + str(loop))
             test = rotate(test, loop * -1)
 
         elif instruction[0] == 'x':
             instruction = instruction[1:].split('/')
             a = instruction[0]
             b = instruction[1]
-            print('Exchange ' + a + ' and ' + b)
+            #print('Exchange ' + a + ' and ' + b)
+            a = int(a)
+            b = int(b)
             test[a], test[b] = test[b], test[a]
 
         elif instruction[0] == 'p':
             instruction = instruction[1:].split('/')
             a = instruction[0]
             b = instruction[1]
-            print('Partner ' + a + ' and ' + b)
-            a = test.find(a)
-            b = test.find(b)
+            #print('Partner ' + a + ' and ' + b)
+            a = test.index(a)
+            b = test.index(b)
             test[a], test[b] = test[b], test[a]
 
     return test
 
 
-def second(input):
-    # Process
-    return 2
+def second(instructions, string):
+    seen = []
+    for i in range(1000000000):
+        s = ''.join(string)
+        if s in seen:
+            return seen[1000000000 % i - 1]
+        seen.append(s)
+        # print('Dance n°' + str(i))
+        string = first(instructions, string)
+    return string
 
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
         content = f.readline()
-    content = content.split(',')
 
-    instructions.split(',')
-    print("1. Le résultat est " + str(first(instructions)))
+    test = False
+    if test:
+        instructions = instructions.split(',')
+        string = ['a', 'b', 'c', 'd', 'e']
+    else:
+        instructions = content.split(',')
+        string = list('abcdefghijklmnop')
 
-    #print("2. Le résultat est " + str(second(content)))
+    result1 = first(instructions, string)
+
+    print("1. Le résultat est " + ''.join(result1))
+    result1000000000 = second(instructions, result1)
+    print("2. Le résultat est " + ''.join(result1000000000))
