986cd9f7d3a94d2b382ef198833cfb269987b660
bbe <benjamin.bernard-bouissieres@ipexia.com>
2018-12-03 18:20:24 +0100

    working on part 2

--- python/2018/day3/day3.py
+++ python/2018/day3/day3.py
@@ -9,24 +9,34 @@ test = """#1 @ 1,3: 4x4
 def first(input):
     fabric = np.zeros((1000, 1000))
 
+    alive = []
     for line in input:
         id, at, pos, size = line.split()
+        id = int(id[1:])
         x, y = pos.split(',')
         y = y[0:-1]  # remove the :
         width, length = size.split('x')
-
         # print('%s:%s - %sx%s' % (x, y, width, length))
         i = int(x)
         while i < int(x) + int(width):
+            if id not in alive:
+                alive.append(id)
             j = int(y)
             while j < int(y) + int(length):
                 if fabric[i,j] != 0:
+                    if fabric[i,j] in alive:
+                        print(int(fabric[i,j]))
+                        alive.remove(int(fabric[i,j]))
+                        print(alive)
+                        if id in alive:
+                            print(id)
+                            alive.remove(id)
                     fabric[i, j] = '-1'
                 else:
-                    fabric[i, j] = id[1:]
+                    fabric[i, j] = id
                 j +=1
             i += 1
-
+    print(alive)
     return (fabric == -1).sum()
 
 
