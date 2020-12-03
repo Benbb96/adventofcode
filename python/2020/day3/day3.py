8efe615df05c8e67c83073779102771ba8e06100
bbe <benjamin.bernard-bouissieres@ipexia.com>
2020-12-03 06:42:53 +0100

    2020 - Day 3 (35 min) - in top 5000

---
+++ python/2020/day3/day3.py
@@ -0,0 +1,48 @@
+def count_trees(x_speed, y_speed, input):
+    tree_encountered = 0
+    x = y = 0
+
+    row_nb = len(input)
+    col_nb = len(input[0])
+
+    while y < row_nb - y_speed:
+        x += x_speed
+        y += y_speed
+        if input[y][x % col_nb] == '#':
+            tree_encountered += 1
+
+    return tree_encountered
+
+
+def first(input):
+    return count_trees(3, 1, input)
+
+
+def multiply_list(l):
+    # Multiply elements one by one
+    result = 1
+    for x in l:
+        result = result * x
+    return result
+
+
+def second(input):
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    slopes_tree_encountered = []
+
+    for slope in slopes:
+        x_speed = slope[0]
+        y_speed = slope[1]
+        slopes_tree_encountered.append(count_trees(x_speed, y_speed, input))
+
+    return multiply_list(slopes_tree_encountered)
+
+
+if __name__ == "__main__":
+    with open('input.txt', 'r') as f:
+        content = f.readlines()
+    content = [list(x.strip()) for x in content]
+
+    print("1. Le résultat est %s" % first(content))
+
+    print("2. Le résultat est %s" % second(content))
