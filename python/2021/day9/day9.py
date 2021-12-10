d706f6234c8f5f60b07207549462f4f67387b83e
bbe <benjamin.bernard-bouissieres@ipexia.com>
2021-12-10 14:02:49 +0100

    2021 - Day 9 (part 2)

--- python/2021/day9/day9.py
+++ python/2021/day9/day9.py
@@ -1,3 +1,5 @@
+from math import prod
+
 from python.download_input import get_input_content
 
 
@@ -5,24 +7,21 @@ ADJACENCY = {(0, 1), (-1, 0), (1, 0), (0, -1)}  # Adjacency matrix
 
 
 # https://newbedev.com/pythonic-and-efficient-way-of-finding-adjacent-cells-in-grid
-def get_adjacent_cells(grid, x, y, max_x, max_y):
+def get_adjacent_cells(grid, x, y):
+    max_x = len(grid[0])
+    max_y = len(grid)
     for dx, dy in ADJACENCY:
         if 0 <= (x + dx) < max_x and 0 <= y + dy < max_y:  # Boundaries check
             # yielding is usually faster than constructing a list and returning it if you're just using it once
-            yield grid[y + dy][x + dx]
+            yield x + dx, y + dy, grid[y + dy][x + dx]
 
 
-def first(data):
-    grid = []
-    for line in data:
-        grid.append(list(map(int, line)))
-
+def first(grid):
     count = 0
     for y, row in enumerate(grid):
         for x, val in enumerate(row):
             is_min = True
-            for neighbour in get_adjacent_cells(grid, x, y, len(row), len(grid)):
-                print(val, neighbour)
+            for n_x, n_y, neighbour in get_adjacent_cells(grid, x, y):
                 if val >= neighbour:
                     is_min = False
                     break
@@ -32,20 +31,34 @@ def first(data):
     return count
 
 
-def second(data):
-    return 2
+def get_basin_size(grid, x, y, already_visited):
+    already_visited.add((x, y))
+    size = 1
+    for n_x, n_y, neighbour in get_adjacent_cells(grid, x, y):
+        if (n_x, n_y) in already_visited or neighbour == 9:
+            continue
+        size += get_basin_size(grid, n_x, n_y, already_visited)
+    return size
+
+
+def second(grid):
+    already_visited = set()
+    basins_size = []
+    for y, row in enumerate(grid):
+        for x, val in enumerate(row):
+            if (x, y) in already_visited or val == 9:
+                continue
+
+            basins_size.append(get_basin_size(grid, x, y, already_visited))
+    basins_size.sort()
+    return prod(basins_size[-3:])
 
 
 if __name__ == "__main__":
     content = get_input_content(__file__)
-    test_input = '''2199943210
-3987894921
-9856789892
-8767896789
-9899965678'''
-    # if test_input:
-    #     content = test_input.split('\n')
 
-    print(f'Le résultat de la première partie est :\n{first(content)}')
+    grid_input = [list(map(int, line)) for line in content]
 
-    print(f'Le résultat de la deuxième partie est :\n{second(content)}')
+    print(f'Le résultat de la première partie est :\n{first(grid_input)}')
+
+    print(f'Le résultat de la deuxième partie est :\n{second(grid_input)}')
