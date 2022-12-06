5c9ac814f4c3712d655e6db833252d17908219fe
Benbb96 <benbb96@gmail.com>
2022-12-06 06:17:18 +0100

    2022 - Day 6 refactored

--- python/2022/day6/day6.py
+++ python/2022/day6/day6.py
@@ -1,30 +1,18 @@
 from python.download_input import get_input_content
 
 
-def first(data):
-    i = 4
-    while i < len(data):
-        if len(set(data[i-4:i])) == 4:
-            print(data[i-4:i])
-            return i
-        i+=1
-
-
-def second(data):
-    i = 14
-    while i < len(data):
-        if len(set(data[i - 14:i])) == 14:
-            print(data[i - 14:i])
+def get_start_of_packet(msg: str, length: int) -> int:
+    i = length
+    while i < len(msg):
+        if len(set(msg[i-length:i])) == length:
             return i
         i += 1
+    raise ValueError(f'Message does not contain a sequence of {length} different characters.')
 
 
 if __name__ == "__main__":
     content = get_input_content(__file__, split_by_lines=False)
-    test_input = '''nppdvjthqldpwncqszvftbrmjlhg'''
-    # if test_input:
-    #     content = test_input
 
-    print(f'Le résultat de la première partie est :\n{first(content)}')
+    print(f'Le résultat de la première partie est :\n{get_start_of_packet(content, 4)}')
 
-    print(f'Le résultat de la deuxième partie est :\n{second(content)}')
+    print(f'Le résultat de la deuxième partie est :\n{get_start_of_packet(content, 14)}')
