2960b3d783ebe080d1179fa690a474d961bde30b
Benbb96 <benbb96@gmail.com>
2022-12-11 12:43:15 +0100

    Create script to init a new day with template file

---
+++ python/init_new_day.py
@@ -0,0 +1,24 @@
+import sys
+from datetime import datetime
+from os import makedirs
+from os.path import exists
+from shutil import copyfile
+
+
+def main(args):
+    day = int(args[1]) if len(args) > 1 else datetime.today().day
+    year = int(args[2]) if len(args) > 2 else datetime.today().year
+
+    if not exists(str(year)):
+        print(f'Create new year {year}')
+        makedirs(str(year))
+
+    if not exists(f'{year}/day{day}'):
+        print(f'Create new day {day} for year {year}')
+        makedirs(f'{year}/day{day}')
+
+    copyfile('template.py', f'{year}/day{day}/day{day}.py')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
