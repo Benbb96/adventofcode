e51c5e6498fd7184e9b69dff40314197e6f98d29
bbe <benjamin.bernard-bouissieres@ipexia.com>
2022-12-16 17:06:58 +0100

    2022 - Day 15 Wip

---
+++ python/2022/day15/day15.py
@@ -0,0 +1,44 @@
+from sys import maxsize
+
+from python.download_input import get_input_content
+
+
+def first(data):
+
+    for line in data:
+        print(line)
+
+    return 1
+
+
+def second(data):
+    return 2
+
+
+if __name__ == "__main__":
+    content = get_input_content(__file__)
+    test_input = '''Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3'''
+    if test_input:
+        content = test_input.split('\n')
+
+    min_x = min_y = maxsize
+    max_x = max_y = -maxsize
+
+
+
+    print(f'Le résultat de la première partie est :\n{first(content)}')
+
+    print(f'Le résultat de la deuxième partie est :\n{second(content)}')
