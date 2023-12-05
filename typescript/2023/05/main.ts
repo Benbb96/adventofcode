3d9f4d4768f07a3cde58b3e8e74e2b9e01602748
bbb <benjamin.bernard-bouissieres@koesio.com>
2023-12-05 15:19:05 +0100

    feat(2023): Day 5 part 1 and start of part 2

---
+++ typescript/2023/05/main.ts
@@ -0,0 +1,48 @@
+import { getAllNumbers } from "../../utils";
+
+
+const file = Bun.file('input');
+const input = await file.text();
+const blocks = input.trimEnd().split('\n\n')
+const seeds = getAllNumbers(blocks[0].split(': ')[1])
+
+let mapper = []
+for (const block of blocks.slice(1)) {
+    const lines = block.split('\n')
+    const rules = []
+    for (const line of lines.slice(1)) {
+        rules.push(getAllNumbers(line))
+    }
+    mapper.push(rules)
+}
+
+const findMinLocationFromSeeds = (seeds: number[], mapper: Array<Array<number>[]>) => {
+    let minLocation = Infinity
+    for (const seed of seeds) {
+        let x = seed
+        for (const rules of mapper) {
+            for (const [dst, src, range] of rules) {
+                if (x >= src && x < src + range) {
+                    x = dst + (x - src)
+                    break
+                }
+            }
+        }
+        if (x < minLocation) {
+            minLocation = x
+        }
+    }
+    return minLocation
+}
+
+console.log(`Result for part 1 is : ${findMinLocationFromSeeds(seeds, mapper)}`)
+
+const seedsPart2: number[] = []
+for (let i = 0; i < seeds.length / 2; i += 2) {
+    for (let j = 0; j < seeds[i + 1]; j++) {
+        seedsPart2.push(seeds[i] + j)
+    }
+}
+// TODO optimize...
+console.log(seedsPart2)
+//console.log(`Result for part 2 is : ${findMinLocationFromSeeds(seedsPart2, mapper)}`)
