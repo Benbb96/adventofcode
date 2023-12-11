7c778e980cf475bf93fcdf7a88b260426e42a9d9
bbb <benjamin.bernard-bouissieres@koesio.com>
2023-12-11 13:53:39 +0100

    feat(2023): Day 8 part 2

--- typescript/2023/08/main.ts
+++ typescript/2023/08/main.ts
@@ -1,33 +1,49 @@
+import { lcm } from "../../utils";
+
 type MatchedLine = {
-    source: string,
-    right: string,
-    left: string
-}
+  source: string;
+  right: string;
+  left: string;
+};
 
-const lineRegex = RegExp(/(?<source>[A-Z]{3}) = \((?<left>[A-Z]{3}), (?<right>[A-Z]{3})\)/)
+const lineRegex = RegExp(
+  /(?<source>[A-Z]{3}) = \((?<left>[A-Z]{3}), (?<right>[A-Z]{3})\)/
+);
 
-const file = Bun.file('input');
+const file = Bun.file("input");
 const input = await file.text();
 
-const [rule, maps] = input.trimEnd().split('\n\n')
-const mapping = new Map()
-for (const line of maps.split('\n')) {
-    const {source, left, right} = line.match(lineRegex)?.groups as MatchedLine
-    mapping.set(source + 'L', left)
-    mapping.set(source + 'R', right)
+const [rule, maps] = input.trimEnd().split("\n\n");
+const mapping = new Map();
+const allSources = [];
+for (const line of maps.split("\n")) {
+  const { source, left, right } = line.match(lineRegex)?.groups as MatchedLine;
+  allSources.push(source);
+  mapping.set(source + "L", left);
+  mapping.set(source + "R", right);
 }
 
-let totalSteps = 0
-let currentPosition = 'AAA'
+let totalSteps = 0;
+let currentPosition = "AAA";
 
-while (currentPosition !== 'ZZZ') {
-    const direction = rule.charAt(totalSteps % rule.length)
-    console.log(direction)
-    currentPosition = mapping.get(currentPosition + direction)
-    console.log(currentPosition)
-    totalSteps++
+while (currentPosition !== "ZZZ") {
+  const direction = rule.charAt(totalSteps % rule.length);
+  currentPosition = mapping.get(currentPosition + direction);
+  totalSteps++;
 }
-console.log(`Result for part 1 is : ${totalSteps}`)
 
-// const total2 = lines.reduce((acc, curr) => acc + handleLine(curr, true), 0)
-// console.log(`Result for part 2 is : ${total2}`)
+console.log(`Result for part 1 is : ${totalSteps}`);
+
+const iterationsBeforeEndingZ = [];
+
+for (let node of allSources.filter((s) => s.endsWith("A"))) {
+  let i = 0;
+  while (!node.endsWith("Z")) {
+    const direction = rule.charAt(i % rule.length);
+    node = mapping.get(node + direction);
+    i++;
+  }
+  iterationsBeforeEndingZ.push(i);
+}
+
+console.log(`Result for part 2 is : ${lcm(iterationsBeforeEndingZ)}`);
--- typescript/utils.ts
+++ typescript/utils.ts
@@ -1,1 +1,14 @@
-export const getAllNumbers = (str: string) => [...str.matchAll(/\d+/g)].map(n => Number(n))
+export const getAllNumbers = (str: string) =>
+  [...str.matchAll(/\d+/g)].map((n) => Number(n));
+
+// Greatest common divisor of 2 integers
+const gcd2 = (a: number, b: number): number => {
+  if (!b) return b === 0 ? a : NaN;
+  return gcd2(b, a % b);
+};
+
+// Least common multiple of 2 integers
+const lcm2 = (a: number, b: number) => (a * b) / gcd2(a, b);
+
+// Least common multiple of a list of integers
+export const lcm = (array: number[]) => array.reduce((acc, n) => lcm2(n, acc));
